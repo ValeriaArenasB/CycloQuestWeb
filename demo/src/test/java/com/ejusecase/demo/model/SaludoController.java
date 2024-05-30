34f0451c1de7032c6cb7324329c69fed332e5abc
Valeria Arenas <valeria.arenas@javeriana.edu.co>
2024-05-30 03:03:45 +0000

    thymeleaf para usar pantallas!!

---
+++ demo/src/test/java/com/ejusecase/demo/model/SaludoController.java
@@ -0,0 +1,13 @@
+package com.ejusecase.demo.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
